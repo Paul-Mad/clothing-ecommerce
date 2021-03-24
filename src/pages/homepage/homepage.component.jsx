ca14637c4cecd19f70d7059c42771c55f11e28be
Paul-Mad <paulin.mad@hotmail.com>
2021-03-24 18:58:33 +0000

    homepage components created and menu-item styled

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "../homepage/homepage.styles.scss";
+import DirectoryComponent from "../../components/directory/directory.component";
+
+const HomePage = () => <DirectoryComponent />;
+
+export default HomePage;
