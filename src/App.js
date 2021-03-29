a62f185358f6bddc9056a4102fbbbec1fcb61628
Paul-Mad <paulin.mad@hotmail.com>
2021-03-29 21:33:22 +0100

    redux implemented, userReducer and userActions added to Header and App components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,50 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import signInAndSignUpPage from "./pages/signin-signup/signin-signup.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import { auth, createUserProfileDocument } from "./firebase/firebase.util";
 
+import { setCurrentUser } from "./redux/user/user.actions";
+
 import "./App.css";
 
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    //When Authentication changes, get the logged-in user and set it to state
+    const { setCurrentUser } = this.props;
+    //When Authentication state changes, get the logged-in user and set it to the App state
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         //Get the created userRef
         const userRef = await createUserProfileDocument(userAuth);
 
         //set the user data to the App state
-        userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: { id: snapShot.id, ...snapShot.data() },
-          });
-
-          //console.log(this.state);
-        });
+        userRef.onSnapshot((snapShot) =>
+          setCurrentUser({ id: snapShot.id, ...snapShot.data() })
+        );
       } else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
 
   componentWillUnmount() {
+    //Call the unsubscribe method when the component is about to unmount to prevent any memory leak in the application related to listeners still being open
     this.unsubscribeFromAuth();
   }
 
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -58,4 +55,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import "./header.styles.scss";
 //import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.util";
+
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -18,6 +23,7 @@ const Header = ({ currentUser }) => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
+
       {currentUser ? (
         <div className="option" onClick={() => auth.signOut()}>
           SIGN OUT
@@ -31,4 +37,4 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+export default connect(mapStateToProps)(Header);
