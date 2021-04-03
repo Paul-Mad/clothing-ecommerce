083a480f33d8c10ff232e3e55d1e50c2fa475854
Paul-Mad <paulin.mad@hotmail.com>
2021-04-03 16:56:50 +0100

    collection-overview.component created

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shopData/shopData.selectors";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-const ShopPage = ({ collections }) => {
+import CollectionsOverview from "../../components/collection-overview/collection-overview.component";
+const ShopPage = () => {
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
